7b3401145800b936fafea5c8ea26b20325112454
pizzalawl <97758804+pizzalawl@users.noreply.github.com>
2024-03-09 22:05:07 -0500

    add files

---
+++ generate.py
@@ -0,0 +1,33 @@
+import ffmpeg
+import sys
+import random
+
+video = sys.argv[1]
+overlay = sys.argv[2]
+length = sys.argv[3]
+
+video_duration = ffmpeg.probe(video)
+overlay_duration = ffmpeg.probe(overlay)
+print(video_duration)
+
+def randomSecondGen(duration):
+    second = random.randint(0, duration)
+
+    return second
+
+"""if __name__ == "__main__":
+    videoRandomSec = randomSecondGen(video_duration)-length
+    overlayRandomSec = randomSecondGen(overlay_duration)-length
+
+    edited_video = (
+        ffmpeg
+        .input(video)
+        .trim(start=videoRandomSec, end=videoRandomSec+length)
+        .output("edited-video.mp4")
+    )
+    edited_overlay = (
+        ffmpeg
+        .input(overlay)
+        .trim(start=overlayRandomSec, end=overlayRandomSec+length)
+        .output("edited-overlay.mp4")
+    )   """   
