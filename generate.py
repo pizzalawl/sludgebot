15332446d106233275c370a67ded71c80f2b2362
pizzalawl <97758804+pizzalawl@users.noreply.github.com>
2024-03-09 22:28:28 -0500

    add info(have to try filters next)

--- generate.py
+++ generate.py
@@ -4,32 +4,33 @@ import random
 
 video = sys.argv[1]
 overlay = sys.argv[2]
-length = sys.argv[3]
+length = int(sys.argv[3])
 
-video_duration = ffmpeg.probe(video)
-overlay_duration = ffmpeg.probe(overlay)
-print(video_duration)
+video_duration = ffmpeg.probe(video)['streams'][0]['duration']
+overlay_duration = ffmpeg.probe(overlay)['streams'][0]['duration']
 
 def randomSecondGen(duration):
-    second = random.randint(0, duration)
+    integerDuration = int(round(float(duration)))
+
+    second = random.randint(0, integerDuration)
 
     return second
 
-"""if __name__ == "__main__":
+if __name__ == "__main__":
     videoRandomSec = randomSecondGen(video_duration)-length
     overlayRandomSec = randomSecondGen(overlay_duration)-length
 
     edited_video = (
         ffmpeg
         .input(video)
-        .trim(start=videoRandomSec, end=videoRandomSec+length)
+        .trim(start=videoRandomSec, end=videoRandomSec+length, duration=length)
         .output("edited-video.mp4")
         .run()
     )
     edited_overlay = (
         ffmpeg
         .input(overlay)
-        .trim(start=overlayRandomSec, end=overlayRandomSec+length)
+        .trim(start=overlayRandomSec, end=overlayRandomSec+length, duration=length)
         .output("edited-overlay.mp4")
         .run()
-    )   """   
+    )
